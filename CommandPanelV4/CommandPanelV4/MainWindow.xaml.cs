5560dd53f2d1c639e346d7a3cdf0ae48e8f1367c
rsanders <sandersrylan2@gmail.com>
2024-05-18 15:08:32 -0500

    Initial Checkin

---
+++ CommandPanelV4/CommandPanelV4/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using CommandPanelV4.Util;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CommandPanelV4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<ServiceObj> ServicesCollection = new ObservableCollection<ServiceObj>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ServicesCollection = new ObservableCollection<ServiceObj>();
+            ServicesListView.ItemsSource = ServicesCollection;
+
+            WindowUtil.ApplyDarkWindowStyle(this);
+
+            ServicesCollection.Add(new ServiceObj() { Name = "XboxGipSvc" });
+        }
+    }
+}
---
+++ CommandPanelV4/CommandPanelV4/ServiceBar.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System.Diagnostics;
+using System.ServiceProcess;
+using System.Timers;
+using System.Windows;
+using System.Windows.Automation.Provider;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace CommandPanelV4
+{
+    /// <summary>
+    /// Interaction logic for ServiceBar.xaml
+    /// </summary>
+    public partial class ServiceBar : UserControl
+    {
+        private const int TIMER_REFRESH_TIME = 1000;
+
+        private System.Timers.Timer refreshTimer;
+
+        public ServiceBar()
+        {
+            InitializeComponent();
+
+            refreshTimer = new System.Timers.Timer(TIMER_REFRESH_TIME);
+
+            refreshTimer.Elapsed += (sender, args)=>Dispatcher.Invoke(RefreshStatus);
+            refreshTimer.Start();
+        }
+
+        private void RefreshStatus() 
+        {
+            //Process? monitoredProcess = Process.GetProcessesByName(localStringName).FirstOrDefault();
+            //if (monitoredProcess != null)
+            //{
+            //    Console.WriteLine("test");
+            //}
+            ServiceObj context=  DataContext as ServiceObj;
+            ServiceController sc = new ServiceController(ServiceName);
+            if (sc != null)
+            {
+                if (sc.Status == ServiceControllerStatus.Running)
+                {
+                    StatusPackIcon.Visibility = Visibility.Visible;
+                    StatusPackIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.Check;
+                    StatusPackIcon.Foreground = Brushes.Green;
+                }
+                else if (sc.Status == ServiceControllerStatus.Stopped)
+                {
+                    StatusPackIcon.Visibility = Visibility.Visible;
+                    StatusPackIcon.Kind = MaterialDesignThemes.Wpf.PackIconKind.Stop;
+                    StatusPackIcon.Foreground = Brushes.Red;
+                }
+                else
+                {
+                    StatusPackIcon.Visibility = Visibility.Hidden;
+                }
+            }
+            
+        }
+
+
+        public string ServiceName
+        {
+            get { return (string)GetValue(ServiceNameProperty); }
+            set { SetValue(ServiceNameProperty, value);}
+        }
+
+        public static readonly DependencyProperty ServiceNameProperty =
+        DependencyProperty.Register("ServiceName", typeof(string), typeof(ServiceBar));
+
+        private void StartServiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceObj context = DataContext as ServiceObj;
+            ServiceController sc = new ServiceController(ServiceName);
+            if (sc != null)
+            {
+                //Note: this requires run as admin 
+                //TODO maybe prevent this using this https://stackoverflow.com/questions/1913429/servicecontroller-permissions ?
+                //Could add an encrypted config for the user name/password for an admin account so I could run a certain thread while impersonating the admin specifically for any admin tasks
+                try
+                {
+                    sc.Start();
+                }
+                catch(InvalidOperationException op)
+                {
+                    MessageBox.Show($"Cannot start service {ServiceName}. Restart this application as an admin!");
+                }
+                
+            }
+        }
+
+        private void StopServiceButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process? monitoredProcess = Process.GetProcessesByName(ServiceName).FirstOrDefault();
+            if (monitoredProcess != null)
+            {
+                monitoredProcess.Kill();
+            }
+        }
+
+        private void RestartServiceButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
